2f3e154f0d543d6f20b2f43a6625848509965086
xkzetsu2 <rosemelo54851@gmail.com>
2026-09-12 18:35:38 -0300

    Corrige aplicacao de plugins no build.gradle.kts do app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
-    alias(libs.plugins.kotlin.serialization) apply false
-    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.hilt)
+    kotlin("kapt")
 }
 
 allprojects {
